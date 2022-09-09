a46b32dc4e236f6376d279f3c1177702dce5a16a
DeCaelo <ludovic.cleuet@gmail.com>
2022-09-09 09:53:11 +0200

    linkedin & github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import reactLogo from "./assets/react.svg";
-import { Toggle } from "./components/Toggle";
-import { useState } from "react";
-import { ToggleAction, ToggleState } from "./hooks/useToggle";
-import { useTheme } from "./hooks/useTheme";
-import Head from "./components/Head";
-import Starfield from "./components/starfield";
-import { motion, useReducedMotion } from "framer-motion";
+import reactLogo from './assets/react.svg';
+import { Toggle } from './components/Toggle';
+import { useState } from 'react';
+import { ToggleAction, ToggleState } from './hooks/useToggle';
+import { useTheme } from './hooks/useTheme';
+import Head from './components/Head';
+import Starfield from './components/starfield';
+import { motion, useReducedMotion } from 'framer-motion';
+import Linkedin from './icons/Linkedin';
+import Github from './icons/Github';
 
 function App() {
   const [bothOn, setBothOn] = useState(false);
   const [timesClicked, setTimesClicked] = useState(0);
-  const [theme, handleChange] = useTheme("dark");
+  const [theme, handleChange] = useTheme('dark');
   const [starfieldSpeed, setStarfieldSpeed] = useState(0.5);
   const shouldReduceMotion = useReducedMotion();
 
+  console.log({ theme });
+  const isDark = theme === 'dark';
+
   function handleToggleChange(state: ToggleState, action: ToggleAction) {
-    if (action.type === "toggle" && timesClicked > 4) {
+    if (action.type === 'toggle' && timesClicked > 4) {
       return;
     }
     setBothOn(state.on);
@@ -26,23 +31,23 @@ function App() {
   function handleResetClick() {
     setBothOn(false);
     setTimesClicked(0);
-    handleChange(theme === "dark");
+    handleChange(theme === 'dark');
   }
 
-  const title = "Développeur React / Typescript";
-  const image = "image";
-  const url = "https://ludovic-cleuet.netlify.app/";
+  const title = 'Développeur React / Typescript';
+  const image = 'image';
+  const url = 'https://ludovic-cleuet.netlify.app/';
   const description =
-    "Bonjour à vous 👋, si vous cherchez un développeur React Typescript en freelance je suis votre homme.";
+    'Bonjour à vous 👋, si vous cherchez un développeur React Typescript en freelance je suis votre homme.';
 
   return (
     <>
       <Head title={title} image={image} url={url} description={description} />
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
         <div>
@@ -52,7 +57,7 @@ function App() {
           <div data-testid="notice">Houlala calme toi!!!</div>
         ) : (
           <div data-testid="click-count">
-            {timesClicked === 1 || timesClicked === 3 ? "🌞" : "🌚"}
+            {timesClicked === 1 || timesClicked === 3 ? '🌞' : '🌚'}
           </div>
         )}
         {timesClicked > 4 ? (
@@ -70,16 +75,16 @@ function App() {
           </a>
         </div>
         <h1 style={{ marginBottom: 100 }}>
-          <motion.span style={{ position: "relative" }}>
+          <motion.span style={{ position: 'relative' }}>
             React / TypeScript Developer
             <motion.div
-              animate={shouldReduceMotion ? {} : { width: ["0%", "100%"] }}
+              animate={shouldReduceMotion ? {} : { width: ['0%', '100%'] }}
               transition={{
                 delay: 0.5,
-                type: "spring",
+                type: 'spring',
                 duration: 1,
               }}
-              initial={{ width: shouldReduceMotion ? "100%" : "0%" }}
+              initial={{ width: shouldReduceMotion ? '100%' : '0%' }}
               className="motion-div"
             />
           </motion.span>
@@ -87,22 +92,39 @@ function App() {
         <div className="skills">
           <pre>
             <code>
-              <span className="let">let</span> skills{" "}
-              <span className="let">=</span> <span className="crochet">[</span>{" "}
+              <span className="let">let</span> skills{' '}
+              <span className="let">=</span> <span className="crochet">[</span>{' '}
               '<span className="body-skills">react</span>
               ', '<span className="body-skills">functional programming</span>
-              ', '<span className="body-skills">ux/ui</span>'{" "}
+              ', '<span className="body-skills">ux/ui</span>'{' '}
               <span className="crochet">]</span>;
             </code>
           </pre>
         </div>
-        <div style={{ marginTop: 100 }}>
+        <div
+          style={{
+            marginTop: 100,
+            display: 'flex',
+            justifyContent: 'space-around',
+          }}
+        >
           <a
             href="https://www.linkedin.com/in/ludovic-cleuet/"
             target="_blank"
-            className="neonText linkedin__link"
+            className="a-link"
           >
-            @ Linkedin Profile
+            <div style={{ marginRight: 10 }}>Linkedin</div>
+            <Linkedin isDark={isDark} />
+          </a>
+          <a
+            href="http://github.com/DeCaelo"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Mon Github pour voir ce que je code"
+            className="a-link"
+          >
+            <div style={{ marginRight: 10 }}>Github</div>
+            <Github isDark={isDark} />
           </a>
         </div>
       </div>
